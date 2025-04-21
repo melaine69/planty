43d2100b1ad684db6013fe6704ad06a3d2ca5ead
Mélaine <113093304+melaine69@users.noreply.github.com>
2025-04-21 13:46:16 +0200

    Augmentation limite memoire php.ini

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -87,9 +87,11 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-if ( ! defined( 'WP_DEBUG' ) ) {
+/*if ( ! defined( 'WP_DEBUG' ) ) {
 	define( 'WP_DEBUG', false );
-}
+}*/
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
 
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
 /* That's all, stop editing! Happy publishing. */
